426afcf7f74a889c3ff2f7a64d2e758996a313e4
eulauragabriel <eulauragabriel@gmail.com>
2023-05-15 21:16:31 -0300

    adicionando app.py com flask implementado, requirements.txt e .gitignore

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('sobremim.html')
+
+@app.route('/sobremim')
+def sobremim():
+    return render_template('sobremim.html')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+@app.route('/capacidades')
+def capacidades():
+    return render_template('capacidades.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
