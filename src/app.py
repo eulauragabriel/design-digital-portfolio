f6ac3e95484eea7972b5d41094db34520c4566bb
eulauragabriel <eulauragabriel@gmail.com>
2023-05-20 22:36:25 -0300

    configurando a página hobbies
    criando a página hobbies, adicionando html, css, imagens referentes e sua rota

--- src/app.py
+++ src/app.py
@@ -2,21 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template('sobremim.html')
+    return render_template("sobremim.html")
 
-@app.route('/sobremim')
+
+@app.route("/sobremim")
 def sobremim():
-    return render_template('sobremim.html')
+    return render_template("sobremim.html")
 
-@app.route('/projetos')
+
+@app.route("/projetos")
 def projetos():
-    return render_template('projetos.html')
+    return render_template("projetos.html")
 
-@app.route('/capacidades')
+
+@app.route("/capacidades")
 def capacidades():
-    return render_template('capacidades.html')
+    return render_template("capacidades.html")
+
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
